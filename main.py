34a6c734b3c66dba0f81a74f0d2ac97b4922e370
Dan Zeinner <drzeinner@gmail.com>
2017-03-08 23:25:22 -0800

    reconstructed the network to allow for easier parameter tweaking

--- main.py
+++ main.py
@@ -88,8 +88,8 @@ def preprocessImages(features, labels):
 # maxPool
 # fullyConnected
 #
-class LayerDef:
-
+# class LayerDef:
+#
     # Data contains the following
     # layerType : string identifying what type of layer to use
     # inTensor  : input tensor to this layer
@@ -98,131 +98,198 @@ class LayerDef:
     # stddev    : standard deviation value for random variable initialization
     # stride    : integer value that determines how far to stride each iteration
     # padding   : string either 'VALID' or 'SAME'
-    def __init__(self, data):
-        if 'layerType' in data:
-            self.layerType = data['layerType']
-        if 'inTensor' in data:
-            self.inTensor  = data['inTensor']
-        if 'shape' in data:
-            self.shape     = data['shape']
-        if 'mean' in data:
-            self.mean      = data['mean']
-        if 'stddev' in data:
-            self.stddev    = data['stddev']
-        if 'stride' in data:
-            self.stride    = data['stride']
-        if 'padding' in data:
-            self.padding   = data['padding']
-        self.layer     = None
+#     def __init__(self, layerType, inTensor, shape=None, mean=None, stddev=None, stride=None, padding=None):
+#         self.layerType = layerType
+#         self.inTensor  = inTensor
+#         self.shape     = shape
+#         self.mean      = mean
+#         self.stddev    = stddev
+#         self.stride    = stride
+#         self.padding   = padding
+#         self.layer     = self.constructLayer()
+#
+#     def constructLayer(self):
+#         constructorMethod = None
+#         try:
+#             constructorMethod = getattr(self, self.layerType)
+#         except AttributeError:
+#             raise NotImplementedError(
+#                 "Class `{}` does not implement `{}`".format(self.__class__.__name__, self.layerType))
+#         return constructorMethod()
+#
+#     # Construct the layer based its settings
+#     def conv2d(self):
+#         weights = tf.Variable(tf.truncated_normal(self.shape, mean=self.mean, stddev=self.stddev))
+#         bias = tf.Variable(tf.zeros(self.shape[3]))
+#         return tf.nn.conv2d(self.inTensor, weights, strides=[1, self.stride, self.stride, 1], padding=self.padding) + bias
+#
+#     # Construct the layer based its settings
+#     def relu(self):
+#         return tf.nn.relu(self.inTensor)
+#
+#     # Construct the layer based its settings
+#     def maxPool(self):
+#         return tf.nn.max_pool(self.inTensor, ksize=self.shape, strides=[1, self.stride, self.stride, 1], padding=self.padding)
+#
+#     # Construct the layer based its settings
+#     def fullyConnected(self):
+#         weights = tf.Variable(tf.truncated_normal(shape=self.shape, mean=self.mean, stddev=self.stddev))
+#         bias = tf.Variable(self.shape[1])
+#         return tf.matmul(self.inTensor, weights) + bias
 
-    def constructLayer(self):
-        constructorMethod = None
-        try:
-            constructorMethod = getattr(self, self.layerType)
-        except AttributeError:
-            raise NotImplementedError(
-                "Class `{}` does not implement `{}`".format(self.__class__.__name__, self.layerType))
-        self.layer = constructorMethod()
+# Create a 2d convolutional layer
+#
+# inTensor  : input tensor to this layer
+# shape     : shape of the layer
+# stride    : integer value that determines how far to stride each iteration
+# padding   : string either 'VALID' or 'SAME'
+# mean      : mean value for random variable initialization
+# stddev    : standard deviation value for random variable initialization
+#
+def conv2d(inTensor, shape, stride, padding, mean, stddev):
+    weights = tf.Variable(tf.truncated_normal(shape, mean=mean, stddev=stddev))
+    bias = tf.Variable(tf.zeros(shape[3]))
+    return tf.nn.conv2d(inTensor, weights, strides=[1, stride, stride, 1], padding=padding) + bias
 
-    # Construct the layer based its settings
-    def conv2d(self):
-        weights = tf.Variable(tf.truncated_normal(self.shape, mean=self.mean, stddev=self.stddev))
-        bias = tf.Variable(tf.zeros(self.shape[3]))
-        return tf.nn.conv2d(self.inTensor, weights, strides=[1, self.stride, self.stride, 1], padding=self.padding) + bias
+# Create a relu layer
+#
+# inTensor  : input tensor to this layer
+#
+def relu(inTensor):
+    return tf.nn.relu(inTensor)
 
-    # Construct the layer based its settings
-    def relu(self):
-        return tf.nn.relu(self.inTensor)
+# Create a max pooling layer
+#
+# inTensor  : input tensor to this layer
+# ksize     : size of the pooling layer
+# stride    : integer value that determines how far to stride each iteration
+# padding   : string either 'VALID' or 'SAME'
+#
+def maxPool(inTensor, ksize, stride, padding):
+    return tf.nn.max_pool(inTensor, ksize=ksize, strides=[1, stride, stride, 1], padding=padding)
 
-    # Construct the layer based its settings
-    def maxPool(self):
-        return tf.nn.max_pool(self.inTensor, ksize=self.shape, strides=[1, self.stride, self.stride, 1], padding=self.padding)
+# Flatten a tensor
+#
+# inTensor  : input tensor to this layer
+#
+def flat(inTensor):
+    return flatten(inTensor)
 
-    # Construct the layer based its settings
-    def fullyConnected(self):
-        weights = tf.Variable(tf.truncated_normal(shape=self.shape, mean=self.mean, stddev=self.stddev))
-        bias = tf.Variable(self.shape[1])
-        return tf.matmul(self.inTensor, weights) + bias
+# Create a fully connected layer
+#
+# inTensor  : input tensor to this layer
+# shape     : shape of the layer
+# mean      : mean value for random variable initialization
+# stddev    : standard deviation value for random variable initialization
+#
+def fullyConnected(inTensor, shape, mean, stddev):
+    weights = tf.Variable(tf.truncated_normal(shape=shape, mean=mean, stddev=stddev))
+    bias = tf.Variable(tf.zeros(shape[1]))
+    return tf.matmul(inTensor, weights) + bias
+
+# Given an array of layer definitions
+# Construct a network and return it
+#
+def constructNetwork(inputTensor, layerData):
+    # Construct the layers
+    layer = inputTensor
+    for ld in layerData:
+        # Construct the layer
+        constructor = ld['constructor']
+        params = ld['params']
+        params['inTensor'] = layer
+        layer = constructor(**params)
+
+    return layer
 
 # -----------------------------------------------------------------
-def DanNet(x):
+# Define a neural network
+#
+def DanNet():
     # Hyperparameters
     mu = 0
     sigma = 0.1
 
+    layerData = []
+    # ----------------------------------------------------------------------------------------------------------
+    # Feature Extraction
+    # Convolution -> MaxPool -> Convolution -> MaxPool
+    #
+    layerData.append({'constructor': conv2d,         'params': {'inTensor': None, 'shape': (5, 5, 3, 6), 'stride': 1, 'padding': "VALID", 'mean': mu, 'stddev': sigma}})
+    layerData.append({'constructor': relu,           'params': {'inTensor': None}})
+    layerData.append({'constructor': maxPool,        'params': {'inTensor': None, 'ksize': (1, 2, 2, 1), 'stride': 2, 'padding': "VALID"}})
+    layerData.append({'constructor': conv2d,         'params': {'inTensor': None, 'shape': (5, 5, 6, 16),'stride': 1, 'padding': "VALID", 'mean': mu, 'stddev': sigma}})
+    layerData.append({'constructor': relu,           'params': {'inTensor': None}})
+    layerData.append({'constructor': maxPool,        'params': {'inTensor': None, 'ksize': (1, 2, 2, 1), 'stride': 2, 'padding': "VALID"}})
+    # ----------------------------------------------------------------------------------------------------------
+    # Classifier
+    # Flattened Extracted Features -> Fully Connected -> Fully Connected -> Fully Connected (Classifier)
+    #
+    layerData.append({'constructor': flat,           'params': {'inTensor': None}})
+    layerData.append({'constructor': fullyConnected, 'params': {'inTensor': None, 'shape': (400, 120), 'mean': mu, 'stddev': sigma}})
+    layerData.append({'constructor': relu,           'params': {'inTensor': None}})
+    layerData.append({'constructor': fullyConnected, 'params': {'inTensor': None, 'shape': (120, 84), 'mean': mu, 'stddev': sigma}})
+    layerData.append({'constructor': relu,           'params': {'inTensor': None}})
+    layerData.append({'constructor': fullyConnected, 'params': {'inTensor': None, 'shape': (84, n_classes), 'mean': mu, 'stddev': sigma}})
 
-    # SOLUTION: Layer 1: Convolutional. Input = 32x32x1. Output = 28x28x6.
-    conv1_data = {
-        'layerType': "conv2d",
-        'inTensor' : x,
-        'shape'    : (5, 5, 3, 6),
-        'mean'     : mu,
-        'stddev'    : sigma,
-        'stride'   : 1,
-        'padding'  : "VALID"
-    }
-    conv1Def = LayerDef(conv1_data)
-    conv1Def.constructLayer()
+    return layerData
 
-    # SOLUTION: Activation.
-    conv1_activation_data = {
-        'layerType'     : "relu",
-        'inTensor' : conv1Def.layer
-    }
-    conv1ActivationDef = LayerDef(conv1_activation_data)
-    conv1ActivationDef.constructLayer()
+# -----------------------------------------------------------------
+# Train a neural net
+#
+def trainNetwork(trainData, epochs, batchSize, learningRate):
+    X_train, y_train = trainData['features'], trainData['labels']
 
-    # SOLUTION: Pooling. Input = 28x28x6. Output = 14x14x6.
-    conv1_maxpool_data = {
-        'layerType'     : "maxPool",
-        'inTensor' : conv1ActivationDef.layer,
-        'shape'    : (1, 2, 2, 1),
-        'stride'  : 2,
-        'padding'  : "VALID"
-    }
-    conv1MaxPoolDef = LayerDef(conv1_maxpool_data)
-    conv1MaxPoolDef.constructLayer()
+    x = tf.placeholder(tf.float32, (None, 32, 32, 3))
+    y = tf.placeholder(tf.int32, (None))
+    one_hot_y = tf.one_hot(y, n_classes)
 
-    # SOLUTION: Layer 2: Convolutional. Output = 10x10x16.
-    conv2_W = tf.Variable(tf.truncated_normal(shape=(5, 5, 6, 16), mean = mu, stddev = sigma))
-    conv2_b = tf.Variable(tf.zeros(16))
-    conv2   = tf.nn.conv2d(conv1MaxPoolDef.layer, conv2_W, strides=[1, 1, 1, 1], padding='VALID') + conv2_b
+    network = constructNetwork(x, DanNet())
+    cross_entropy = tf.nn.softmax_cross_entropy_with_logits(logits=network, labels=one_hot_y)
+    loss_operation = tf.reduce_mean(cross_entropy)
+    optimizer = tf.train.AdamOptimizer(learning_rate=learningRate)
+    trainer = optimizer.minimize(loss_operation)
 
-    # SOLUTION: Activation.
-    conv2 = tf.nn.relu(conv2)
+    correct_prediction = tf.equal(tf.argmax(network, 1), tf.argmax(one_hot_y, 1))
+    accuracy_operation = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
 
-    # SOLUTION: Pooling. Input = 10x10x16. Output = 5x5x16.
-    conv2 = tf.nn.max_pool(conv2, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='VALID')
+    saver = tf.train.Saver()
 
-    # SOLUTION: Flatten. Input = 5x5x16. Output = 400.
-    fc0   = flatten(conv2)
+    with tf.Session() as sess:
+        sess.run(tf.global_variables_initializer())
+        num_examples = len(X_train)
 
-    # SOLUTION: Layer 3: Fully Connected. Input = 400. Output = 120.
-    fc1_W = tf.Variable(tf.truncated_normal(shape=(400, 120), mean = mu, stddev = sigma))
-    fc1_b = tf.Variable(tf.zeros(120))
-    fc1   = tf.matmul(fc0, fc1_W) + fc1_b
+        print("Training...")
+        print("Params: epochs:{}\tbatchSize:{}\tlearningRate:{}".format(epochs, batchSize, learningRate))
+        print()
+        for i in range(epochs):
+            X_train, y_train = shuffle(X_train, y_train)
+            for offset in range(0, num_examples, batchSize):
+                end = offset + batchSize
+                batch_x, batch_y = X_train[offset:end], y_train[offset:end]
+                sess.run(trainer, feed_dict={x: batch_x, y: batch_y})
 
-    # SOLUTION: Activation.
-    fc1    = tf.nn.relu(fc1)
+            validation_accuracy = evaluate(X_valid, y_valid, accuracy_operation, x, y)
+            print("EPOCH {} ...".format(i + 1))
+            print("Validation Accuracy = {:.3f}\n".format(validation_accuracy))
 
-    # SOLUTION: Layer 4: Fully Connected. Input = 120. Output = 84.
-    fc2_W  = tf.Variable(tf.truncated_normal(shape=(120, 84), mean = mu, stddev = sigma))
-    fc2_b  = tf.Variable(tf.zeros(84))
-    fc2    = tf.matmul(fc1, fc2_W) + fc2_b
+        saver.save(sess, './lenet')
+        print("Model saved")
 
-    # SOLUTION: Activation.
-    fc2    = tf.nn.relu(fc2)
-
-    # SOLUTION: Layer 5: Fully Connected. Input = 84. Output = 10.
-    fc3_W  = tf.Variable(tf.truncated_normal(shape=(84, n_classes), mean = mu, stddev = sigma))
-    fc3_b  = tf.Variable(tf.zeros(n_classes))
-    logits = tf.matmul(fc2, fc3_W) + fc3_b
-
-    return logits
+# --------------------------------------------------------------------------------------------------------
+# Test the network
+#
+def testNetwork(testData):
+    X_test, y_test = testData['features'], testData['labels']
+    saver = tf.train.Saver()
+    with tf.Session() as sess:
+        saver.restore(sess, tf.train.latest_checkpoint('.'))
+        test_accuracy = evaluate(X_test, y_test)
+        print("Test Accuracy = {:.3f}".format(test_accuracy))
 
 # --------------------------------------------------------------------------------------------------------
 #
-def evaluate(X_data, y_data):
+def evaluate(X_data, y_data, accuracy_operation, x, y):
     num_examples = len(X_data)
     total_accuracy = 0
     sess = tf.get_default_session()
@@ -240,11 +307,8 @@ X_valid, y_valid = valid['features'], valid['labels']
 X_test, y_test = test['features'], test['labels']
 
 n_classes = np.max(y_train + 1)
-image_shape = [X_train.shape[1], X_train.shape[2]]
 
 print("Number of training examples =", X_train.shape[0])
-print("Number of testing examples =", X_test.shape[0])
-print("Image data shape =", image_shape)
 print("Number of classes =", n_classes)
 
 # X_train_processed, y_train = preprocessImages(X_train, y_train)
@@ -255,49 +319,9 @@ print("Number of classes =", n_classes)
 # print('max={}, min={}, mean={}, std={}'.format(np.max(X_valid_processed), np.min(X_valid_processed),
 #                                                np.mean(X_valid_processed), np.std(X_valid_processed)))
 
-EPOCHS = 10
+EPOCHS = np.arange(1, 20, 4)
 BATCH_SIZE = 128
 learningRate = 0.001
 
-# Set up
-x = tf.placeholder(tf.float32, (None, 32, 32, 3))
-y = tf.placeholder(tf.int32, (None))
-one_hot_y = tf.one_hot(y, n_classes)
-
-
-logits = DanNet(x)
-cross_entropy = tf.nn.softmax_cross_entropy_with_logits(logits=logits, labels=one_hot_y)
-loss_operation = tf.reduce_mean(cross_entropy)
-optimizer = tf.train.AdamOptimizer(learning_rate = learningRate)
-training_operation = optimizer.minimize(loss_operation)
-
-
-correct_prediction = tf.equal(tf.argmax(logits, 1), tf.argmax(one_hot_y, 1))
-accuracy_operation = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
-saver = tf.train.Saver()
-
-with tf.Session() as sess:
-    sess.run(tf.global_variables_initializer())
-    num_examples = len(X_train)
-
-    print("Training...")
-    print()
-    for i in range(EPOCHS):
-        X_train, y_train = shuffle(X_train, y_train)
-        for offset in range(0, num_examples, BATCH_SIZE):
-            end = offset + BATCH_SIZE
-            batch_x, batch_y = X_train[offset:end], y_train[offset:end]
-            sess.run(training_operation, feed_dict={x: batch_x, y: batch_y})
-
-        validation_accuracy = evaluate(X_valid, y_valid)
-        print("EPOCH {} ...".format(i + 1))
-        print("Validation Accuracy = {:.3f}\n".format(validation_accuracy))
-
-    saver.save(sess, './lenet')
-    print("Model saved")
-
-# with tf.Session() as sess:
-#     saver.restore(sess, tf.train.latest_checkpoint('.'))
-#
-#     test_accuracy = evaluate(X_test, y_test)
-#     print("Test Accuracy = {:.3f}".format(test_accuracy))
+for numEpochs in EPOCHS:
+    trainNetwork(train, numEpochs, BATCH_SIZE, learningRate)
